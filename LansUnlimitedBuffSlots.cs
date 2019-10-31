8f43812e636ea94fce4de25ba7c58054158288de
Lanboost <lanboost@hotmail.com>
2019-10-31 17:59:30 +0100

    Second

---
+++ LansUnlimitedBuffSlots.cs
@@ -0,0 +1,16 @@
+using Terraria.ModLoader;
+
+namespace LansUnlimitedBuffSlots
+{
+	public class LansUnlimitedBuffSlots : Mod
+	{
+
+		public override uint ExtraPlayerBuffSlots => 200;
+
+		public LansUnlimitedBuffSlots()
+		{
+		}
+
+	
+	}
+}
